cb5044a2face80a33c1fd535e8e181ce8715aa39
Florian Boudzoumou <florian.boudzoumou@gmail.com>
2021-04-23 11:18:42 +0200

    Ajout des bordures, modification des textes et ouvertures a plusieurs formats photo

--- galerie.sh
+++ galerie.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-image=`ls *.jpg`
+#Creation de la liste dans la variable image
+
+image=`ls *.jpg *jpeg *png 2>/dev/null`
+
+#Creation du dossier miniature si inexistant
 
 if [ ! -e miniature ]
 then
 	mkdir miniature
 fi
+
+#Creation du debut du code HTML
 
 echo "<!DOCTYPE html>
 <html>
@@ -17,27 +23,30 @@ echo "<!DOCTYPE html>
 	<body>
 		<header>
 			<h1>Generateur de galerie d'image</h1>
-			<h2>Je suis un script VIM. Je recupere les images du dossier dans lequel je me 
-			me trouve, et je les affiche sur cette page !!</h2>
+			<h2>Je suis un script VIM. Je recupere les images du dossier dans lequel tu  
+			te trouve,cree des miniatures, et je les affiche sur cette page !!</h2>
 		</header>
 
-		<section><div> 
+		<section> 
 	"	>galerie.html
 
+#Implementation des images dans le code HTML
 
 for i in $image
 do
 
-	convert -thumbnail 50x50 $i $i.min 
+	convert -thumbnail 100x100 $i $i.min 
 	 mv $i.min miniature 
-	echo "<p>Image:$i</br>
-	<a href=\"$i\"><img src=\"miniature/$i.min\" alt=\"imagetest\" /></a></p></div>">>galerie.html
-
+	echo "<div><p>Image:$i</p>
+	<p><a href=\"$i\"><img src=\"miniature/$i.min\" alt=\"imagetest\" /></a></p></div>">>galerie.html
 
 done
 
+#Fin du code HTML
+
 echo -e "</section>\n</body>\n</html>">>galerie.html
 
+#Creation du code CSS
 
 echo "
 body
@@ -45,6 +54,7 @@ body
 	width:800px;
 	margin:auto;
 	text-align:center;
+	background-color: beige;
 }
 
 section
@@ -61,6 +71,28 @@ section
 div
 {
 	flex-direction: column;
+	border: 2px black solid;
+	margin: 5px 5px;
+	overflow: hidden;
+	width:10em;
+
+}
+
+h1
+{
+	text-transfom:uppercase;
+}
+
+h2
+{
+	font-style: italic;
+	font-size:medium;
 }
 	
+header
+{
+	height: 200px;	
+
+}
+
 ">galerie.css
